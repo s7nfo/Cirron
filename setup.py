9445924e93f1b57e055745e97fdca162cba1c469
Matt Stuchlik <matej.stuchlik@gmail.com>
2024-05-15 22:23:00 +0200

    Update setup.py for uploading to pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="Cirron",
     version="0.2",
-    packages=["cirron"],
+    packages=find_packages(),
     package_data={
         "cirron": ["cirronlib.cpp", "apple_arm_events.h", "tracer.py", "cirron.py"],
     },
+    author="Matt Stuchlik",
+    author_email="matej.stuchlik@gmail.com",
+    description="Cirron measures how many CPU instructions and system calls a piece of Python code executes.",
+    url="https://github.com/s7nfo/Cirron",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
