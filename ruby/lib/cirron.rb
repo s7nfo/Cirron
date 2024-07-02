1e21f002c3b840a6da1443b06f3f1bd9d85b9f32
Matt Stuchlik <matej.stuchlik@gmail.com>
2024-07-02 12:56:41 +0800

    .

--- ruby/lib/cirron.rb
+++ ruby/lib/cirron.rb
@@ -1,14 +1,2 @@
 require_relative 'tracer'
 require_relative 'collector'
-
-if FILE == $0
-  puts "Testing Tracer class..."
-  tracer = Tracer.trace do
-    puts "Hello, World!"
-    sleep(0.1)
-  end
-  puts "Trace completed. First 5 events:"
-  puts tracer.trace.first(5)
-  puts "Test completed."
-end
-puts "Tracer class loaded successfully."
