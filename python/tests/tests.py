88dcd74c7a30bf8155f047a572fb80e9b757ea06
Matt Stuchlik <matej.stuchlik@gmail.com>
2024-07-02 11:40:52 +0800

    pls gh actions

--- python/tests/tests.py
+++ python/tests/tests.py
@@ -13,7 +13,9 @@ class Test(unittest.TestCase):
         self.assertEqual(len(t.trace), 3)
 
     @unittest.skipIf(
-        os.getenv("GITHUB_ACTIONS") == "true",
+        os.getenv("POWERSHELL_DISTRIBUTION_CHANNEL").startswith(
+            "GitHub"
+        ),  # GITHUB_ACTIONS "should" be defined, but turns out it's not.
         "As of 02/07/2024, GitHub Actions does not support perf_event_open.",
     )
     def test_collector(self):
