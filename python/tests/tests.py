a22657fed3a4a6b27cbe1fade87317c96336bcd6
Matt Stuchlik <matej.stuchlik@gmail.com>
2024-07-02 11:33:07 +0800

    pls gh actions

--- python/tests/tests.py
+++ python/tests/tests.py
@@ -13,7 +13,7 @@ class Test(unittest.TestCase):
         self.assertEqual(len(t.trace), 3)
 
     @unittest.skipIf(
-        "GITHUB_ACTIONS" in os.environ,
+        os.getenv("GITHUB_ACTIONS") == "true",
         "As of 02/07/2024, GitHub Actions does not support perf_event_open.",
     )
     def test_collector(self):
